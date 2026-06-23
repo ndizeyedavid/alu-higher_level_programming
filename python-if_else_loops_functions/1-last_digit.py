75ca75948bdfc13abacda912d6e3ead40df48dfb
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-06-23 12:11:57 +0200

    completed task #1 - printing status of the last digit of a number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(str(number)[-1])
+result = ""
+
+if last_digit > 5:
+    result = "greater than 5"
+elif last_digit == 0:
+    result = "0"
+elif last_digit < 6:
+    result = "less than 6 and not 0"
+    
+print(f"Last digit of {number} is {last_digit} and is {result}")
