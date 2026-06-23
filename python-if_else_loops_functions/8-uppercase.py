14b69711a8380dc807219b56b5601b7d42486c6e
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-06-23 15:06:29 +0200

    fixed the issue for the case of quotations

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,5 @@ def uppercase(str):
         c = str[i]
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-        print("{:s}".format(c), end="" if i < len(str) - 1 else "\n")
+        print("{:s}".format(c), end="")
+    print()
