06d721c2eab2c0a0480874ef08ac8dfb63b28feb
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-06-24 20:55:16 +0200

    completed advanced #14 - used a for loop and a decrementation approach together with a formated string

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+result = ""
+for i in range(26):
+    if i % 2 == 0:
+        result += chr(122 - i)
+    else:
+        result += chr(122 - i - 32)
+print("{:s}".format(result))
