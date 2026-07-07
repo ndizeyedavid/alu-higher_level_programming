91e6114327aad6603545437e372732e9e7903159
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-07 12:42:14 +0200

    final fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for numbers in matrix:
-        for i  in range(len(numbers)):
+        for i in range(len(numbers)):
             print("{:d}".format(numbers[i]), end=" " if i < len(numbers) - 1 else "")
         print()
 
