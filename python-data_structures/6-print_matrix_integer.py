8ec6b3db6889e02564588c9853cac1a7fbad67a5
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-07 12:31:52 +0200

    completed task #6 - i didnt know print() alone creates a new line without extras

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for numbers in matrix:
         for number in numbers:
             print("{:d}".format(number), end=" ")
-        print("\n")
+        print()
