8d0b52bec951d551df80fd42fdf3b21cafc5de76
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-07 12:35:08 +0200

    fixed an error in the intrepretation of the number

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for numbers in matrix:
         for number in numbers:
-            print("{:d}".format(number), end=" ")
+            print("{:d}".format(number), end=" " if number < numbers[len(numbers) - 1] else "")
         print()
