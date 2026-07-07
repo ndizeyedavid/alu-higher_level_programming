629cad3c17a05f4509b00cc1105f5661d9e1feba
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-07 12:22:28 +0200

    fixed the implication of the str.format() with {:d}

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for numbers in matrix:
         for number in numbers:
-            print("{}".format(number), end=" ")
+            print("{:d}".format(number), end=" ")
         print("\n")
