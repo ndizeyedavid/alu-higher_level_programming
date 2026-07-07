b95e933fc63c4f8158e3c8b87163103e46e855cc
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-07 12:40:47 +0200

    revamped the overall structure of the matrix print function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for numbers in matrix:
-        for number in numbers:
-            print("{:d}".format(number), end=" " if number < numbers[len(numbers) - 1] else "")
+        for i  in range(len(numbers)):
+            print("{:d}".format(numbers[i]), end=" " if i < len(numbers) - 1 else "")
         print()
+
+atrix = [
+    [1, 2],
+    [4, 5]
+]
+print_matrix_integer(atrix)
