dd10081e011deb542612d22945b4f7a43c1202f9
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-29 15:02:10 +0200

    fix PEP8 E501 line too long in 0 and 4-hbtn_status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,7 +4,8 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    url = "https://alu-intranet.hbtn.io/status"
+    r = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
