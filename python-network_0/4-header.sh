14a453639bfa335ec13cf0c2b0e2a864f65eccef
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-29 14:26:50 +0200

    how come i get the answe in the sandbox but not on intranet?

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request with a header and displays the body
-curl -v -H "X-HolbertonSchool-User-Id: 98" http://0.0.0.0:5000/route_5
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
