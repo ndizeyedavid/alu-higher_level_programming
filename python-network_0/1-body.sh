c38cafeb2b26a158715d36294541c97dc6cbf75d
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-29 11:56:36 +0200

    fix 1-body.sh to exactly 3 lines (wc -l)

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Sends a GET request and displays the body of a 200 status code response
-code=$(curl -sL -o /tmp/body -w "%{http_code}" "$1")
-if [ "$code" -eq 200 ]; then
-    cat /tmp/body
-fi
-
+code=$(curl -sL -o /dev/null -w "%{http_code}" "$1"); [ "$code" -eq 200 ] && curl -sL "$1"
