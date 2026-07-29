60acc2884cfe91c9a6d03590791b65c6b6ed2027
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-29 11:49:24 +0200

    added multiple freakin break line on all files cause intranet said so

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request and displays the body of a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"\n
--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request with a header and displays the body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"\n
