eba8f3aef9c1f31c323d227ebd969a45f850ca9d
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-29 15:06:21 +0200

    fix URL to intranet.hbtn.io (DNS resolution issue)

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using urllib."""
+"""Fetches https://intranet.hbtn.io/status using urllib."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using requests."""
+"""Fetches https://intranet.hbtn.io/status using requests."""
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
     r = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
